a4868d86d56b432bb135b0b01234d9d631a9e6a3
Ronny Pfannschmidt <opensource@ronnypfannschmidt.de>
2011-08-26 23:44:51 +0200

    add a more detailed parse test

---
+++ testing/test_parse_command.py
@@ -0,0 +1,19 @@
+from whenever import parse_command
+commands = [
+    'defer (1) again (2) 3;',
+    'again (1) defer (3 || N(1)<=N(2) || N(7)>99) 2#N(1),3,7;'
+]
+
+
+def pytest_generate_tests(metafunc):
+    for command in commands:
+        metafunc.addcall(id=command, funcargs={"command": command})
+
+def test_parse_command(command):
+    try:
+        parse_command(command)
+    except Exception, e:
+        print command
+        print ' '*e.args[0].i + '^'
+        raise
+
